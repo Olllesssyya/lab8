1eab70ac46166b083ab8c06b299296f3289ac360
Olesia Fesiuk <O.Fesyuk_FIT_9_22_B_d@knute.edu.ua>
2022-10-18 20:55:27 +0300

    перший реліз

--- variant8-zavdannya1_lab8.py
+++ variant8-zavdannya1_lab8.py
@@ -1,8 +1,9 @@
 #Лабароторна 8 варіант 8 завдання1. Посилання на GitHub https://github.com/Olllesssyya/Lab8
+
 from array import *
 import random
 import math
-s = 0
+s = 1
 c = 0
 
 N=int(input("Задайте кількість елементів масиву: "))
@@ -11,16 +12,18 @@ arr=array('i',[])
 for i in range(N):
     x=random.randint(0,10)
     arr.append(x)
-    print(arr[i])
+
+print(arr)
 
 for j in arr:
     if j%2==0:
-        print("J=",j)
-        if s>0:
-            s=s*j
-            c+=1
-        else:
-            s=j
-            c+=1   
-print("Середне геометричне дорівнює: ",math.pow(s, (1/c))) #корінь з s в ступені с
-print(c)
+        print("парний елемент =",j)
+        s=s*j
+        c+=1
+           
+if c>0:
+    print("Середнє геометричне дорівнює: ",math.pow(s, (1/c)))
+else:
+    print("Масив не містить парних чисел")
+    
+
